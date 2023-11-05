881f41245c707816befdd5d2b7551aaf3af287eb
karar <karar@KARAPC>
2023-11-05 00:19:25 -0400

    I did very good job at asking questions EZZZ

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+const chatHeader = document.getElementById('chat-header'); 
+const socket = io('http://localhost:3000');
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container'); 
+const messageInput = document.getElementById('message-input');
 
-const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
+const name = prompt('What is your name?');
+appendMessage('You joined');
+socket.emit('new-user', name);
 
-socket.on('error', (error) => {
-    console.error('Socket.IO error:', error);
+socket.on('room-question', question => {
+    chatHeader.innerText = question; 
 });
+
 
 socket.on('chat-message', data => {
     appendMessage(`${data.name}: ${data.message}`) 
+
 })
 
 socket.on('user-connected', name => {
@@ -21,7 +24,7 @@ socket.on('user-connected', name => {
 
 socket.on('user-disconnected', name => {
     appendMessage(`${name} disconnected`);
-});
+}); 
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault() // stop page from posting to server
@@ -32,7 +35,9 @@ messageForm.addEventListener('submit', e => {
 })
 
 function appendMessage(message) {
-    const messageElement = document.createElement('div')
-    messageElement.innerText = message
-    messageContainer.append(messageElement)
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+
+    messageContainer.scrollTop = messageContainer.scrollHeight;
 }
--- server.js
+++ server.js
@@ -8,17 +8,39 @@ const io = socketIO(server);
 
 const users = {}
 
-app.use(express.static(__dirname)); // Serve static files from the current directory
+const questions = [
+    { question: "What is 2+2", answer: "4" },
+    // ...other questions
+];
+let currentQuestion = null;
+let questionAnswered = false;
+
+app.use(express.static(__dirname)); 
 
 io.on('connection', socket => {
     socket.on('new-user', name => {
-        users[socket.id] = name;
+        users[socket.id] = { name: name, points: 0 };
+        if (!currentQuestion) {
+            currentQuestion = questions[Math.floor(Math.random() * questions.length)];
+            questionAnswered = false;
+            io.emit('room-question', currentQuestion.question); // Send to all users
+        } else {
+            socket.emit('room-question', currentQuestion.question); // Send only to the new user
+        }
         socket.broadcast.emit('user-connected', name);
+
     });
 
     socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] } ); // sends the message to all other users
-    });
+        if (message.toLowerCase() === currentQuestion.answer.toLowerCase() && !questionAnswered) {
+
+            questionAnswered = true;
+            users[socket.id].points += 1; // Increment points for the user who answered correctly
+            io.emit('correct-answer', { name: users[socket.id].name, points: users[socket.id].points }); // Announce the correct answer
+            // Optionally, set a timeout to send a new question after some time
+        } 
+            socket.broadcast.emit('chat-message', { message: message, name: users[socket.id].name });
+            });
 
     socket.on('disconnect', () => {
         socket.broadcast.emit('user-disconnected', users[socket.id])
